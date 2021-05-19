66522a0dc63824d9f1714cbe9d20681f1735ce6d
gongpengjun <frank.gongpengjun@gmail.com>
2021-05-19 11:02:14 +0800

    initial version of log-nmt-diff.sh

--- log-nmt-diff.sh
+++ log-nmt-diff.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-while true
+PID=$(pgrep java)
+COUNT=60
+INTERVAL=10
+
+jcmd ${PID} VM.native_memory summary | grep "(reserved=" | sed 's/- *\(.*\) (reserved=.*/\1/' | sed 's/ /_/g' | xargs echo Time | sed 's/ /,/g'
+
+COUNTER=0
+while [ $COUNTER -lt $COUNT ];
 do
-  jcmd $(pgrep java) VM.native_memory summary.diff >> /tmp/app-nmt.log
-  sleep 10
+  jcmd ${PID} VM.native_memory summary scale=KB | grep "(reserved=" | sed 's/.*committed=\([0-9]*KB\).*/\1/' | xargs echo $(date +'%H:%M:%S') | sed 's/ /,/g'
+  sleep ${INTERVAL}
+  let COUNTER=COUNTER+1
 done
 
